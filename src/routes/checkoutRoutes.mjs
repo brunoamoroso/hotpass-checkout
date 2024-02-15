68b17b3c3687a2b61ae875c2db377f9d641ef88d
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-02-15 10:18:59 -0300

    Feature backend created
    Created the deleteCard backend, alert message for rendering success and error, cleaned some other files

--- src/routes/checkoutRoutes.mjs
+++ src/routes/checkoutRoutes.mjs
@@ -1,16 +1,15 @@
 import express from 'express';
 import CheckoutController from '../controllers/checkoutController.mjs'
-import identifyValidateDate from '../middlewares/validators/identifyValidator.mjs';
 
 const router = express.Router();
 
-// router.get("/", CheckoutController.identify);
+router.get('/deleteCard/:customerId/:cardId', CheckoutController.deleteCard);
+router.get('/newCard/:id', CheckoutController.newCard);
 router.get("/:botName/:id/:itemId", CheckoutController.identify);
 
-router.post('/identify', identifyValidateDate, CheckoutController.identifyPost);
+router.post('/identify', CheckoutController.identifyPost);
 router.post('/address', CheckoutController.addressPost);
 router.post('/payment', CheckoutController.paymentPost);
 router.post('/confirmPayment', CheckoutController.confirmPayment);
-router.get('/newCard/:id', CheckoutController.newCard);
 
 export default router;
