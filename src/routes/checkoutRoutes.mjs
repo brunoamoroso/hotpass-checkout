585666e2067d601ad0ed80020ff2f372bb0eef5c
bruno-connectabil <pamoroso.bruno@gmail.com>
2024-03-06 07:33:35 -0300

    when customer exists redirec to the choosePayment layout

--- src/routes/checkoutRoutes.mjs
+++ src/routes/checkoutRoutes.mjs
@@ -11,7 +11,7 @@ router.get("/success", CheckoutController.success);
 router.post('/identify', CheckoutController.identifyPost);
 router.post('/address', CheckoutController.addressPost);
 // router.post('/choosePayment', CheckoutController.choosePayment);
-router.post('/payment', CheckoutController.paymentPost);
+router.post('/payment', CheckoutController.choosePaymentPost);
 //before was the creating the first card or the same as newCard
 // router.post('/payment', CheckoutController.paymentPost);
 router.post('/confirmPayment', CheckoutController.confirmPayment);
